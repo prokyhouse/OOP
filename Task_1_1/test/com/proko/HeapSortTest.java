92f6b9e771471a5b2c45c7ba271a2a4d61e9a419
Anastasiya Shadrina <a.shadrina5@mail.ru>
2020-09-10 18:45:02 +0700

    Add first task changes

---
+++ Task_1_1/test/com/proko/HeapSortTest.java
@@ -0,0 +1,46 @@
+package com.proko;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class HeapSortTest {
+
+    @Test
+    public void test1() {
+        int input[] = new int[]{5, 4, 3, 2, 1};
+        int answer[] = new int[]{1, 2, 3, 4, 5};
+        HeapSort ob = new HeapSort();
+        ob.sort(input);
+        Assert.assertArrayEquals(answer, input);
+
+    }
+
+    @Test
+    public void test2() {
+        int input[] = new int[]{1000000000, 4, 3, 2, 1};
+        int answer[] = new int[]{1, 2, 3, 4, 1000000000};
+        HeapSort ob = new HeapSort();
+        ob.sort(input);
+        Assert.assertArrayEquals(answer, input);
+
+    }
+
+    @Test
+    public void test3() {
+        int input[] = new int[]{5, -1, 3, -30, 1};
+        int answer[] = new int[]{-30, -1, 1, 3, 5};
+        HeapSort ob = new HeapSort();
+        ob.sort(input);
+        Assert.assertArrayEquals(answer, input);
+    }
+
+    @Test
+    public void test4() {
+        int input[] = new int[]{5};
+        int answer[] = new int[]{5};
+        HeapSort ob = new HeapSort();
+        ob.sort(input);
+        Assert.assertArrayEquals(answer, input);
+    }
+}
