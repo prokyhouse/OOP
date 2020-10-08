0c6362633dde76f2c6b9b4149433f12d91de0069
Кирилл Прокофьев <prokyhouse@mail.ru>
2020-10-08 21:13:33 +0700

    New realization of Stack using generics (also with iterator)

--- Task-2-1/src/main/java/MyStack.java
+++ Task-2-1/src/main/java/MyStack.java
@@ -1,56 +1,97 @@
 
-public class MyStack {
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-    private Object[] arr;
-    private int size;
-    private int position;
+/**
+ * Class that implements the work of the stack.
+ *
+ * @param <T> type of the elements.
+ */
+
+public class MyStack<T> {
+
+    private StackElement<T> positionStackElement = null;
+    private StackElement<T> head = null;
+    private int position = 0;
 
     /**
-     * @param size stack capacity
+     * Method that pushes the element to the stack.
+     *
+     * @param element element that we need to put in the stack.
      */
-    public MyStack(int size) {
-        this.position = 0;
-        this.size = size;
-        this.arr = new Object[size];
+    public void push(T element) {
+        position++;
+        positionStackElement = new StackElement<>(element, positionStackElement);
+        head = positionStackElement;
     }
 
     /**
-     * @param o smth that needed to push into stack
+     * Method that gets element from the stack.
+     *
+     * @return element of the stack if it exists, otherwise throws exception.
+     * @throws IndexOutOfBoundsException throws this exception if the stack is empty.
      */
-    public void push(Object o) {
-        try {
-        arr[position++] = o;
-        } catch (java.lang.ArrayIndexOutOfBoundsException e){
-            System.out.println("Stack is full");
-            return;
+    public T pop() throws IndexOutOfBoundsException {
+        if (position == 0) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            T element = positionStackElement.getElement();
+            positionStackElement = positionStackElement.getPreviousElement();
+            if (positionStackElement != null) {
+                positionStackElement.setNextElement(null);
+            }
+            position--;
+            if (position == 0) {
+                head = null;
+            }
+            return element;
         }
     }
 
-    public Object pop() {
+    /**
+     * Implementation of the Iterator for the stack.
+     *
+     * @return next element of the stack.
+     */
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private StackElement<T> current = head;
 
+            public boolean hasNext() {
+                return current != null;
+            }
 
-        try {
-            return arr[--position];
-        } catch (java.lang.ArrayIndexOutOfBoundsException e){
-        System.out.println("Stack is full");
-        System.out.println("Stack is empty");
-        return null;
+            public T next() throws NoSuchElementException {
+                if (position == 0) {
+                    throw new EmptyStackException();
+                }
+                T result = current.getElement();
+                if (!hasNext()) {
+                    throw new NoSuchElementException("End of the stack.");
+                }
+                current = current.getNextElement();
+                return result;
+            }
+        };
     }
+
+
+    /**
+     * Check if the stack is empty.
+     *
+     * @return 1 - if the stack is empty, otherwise returns 0.
+     */
+    public boolean isEmpty() {
+        return position == 0;
     }
 
     /**
-     * display our stack
+     * Method that gets amount of elements in the stack.
+     *
+     * @return amount of pushed elements in the stack.
      */
-    public void printStack() {
-        for (int i = 0; i < position; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    /**
-     * @return number of pushed objects
-     */
-    public int count(){
+    public int count() {
         return position;
     }
 }
--- Task-2-1/src/test/java/MyStackTests.java
+++ Task-2-1/src/test/java/MyStackTests.java
@@ -1,38 +1,101 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class MyStackTests {
 
+    //Test with Integers
     @Test
     public void test1() {
-
-        MyStack st = new MyStack(5);
-        st.push(2);
-        st.push(7);
-        st.pop();
-        Assert.assertEquals(1, st.count());
+        MyStack<Integer> stack;
+        stack = new MyStack<Integer>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 1; i < 100; ++i) {
+            if ((i * 5) % 4 == 0) {
+                stack.push(i);
+                arr.add(i);
+            } else {
+                if (arr.size() != 0) {
+                    int elementArr = arr.remove(arr.size() - 1);
+                    int elementStack = stack.pop();
+                    Assert.assertEquals(elementArr, elementStack);
+                }
+            }
+        }
+        Assert.assertEquals(arr.size(), stack.count());
     }
 
+    //Test with Floats
     @Test
     public void test2() {
-        MyStack st = new MyStack(0);
-        st.push(2);
-        st.push(7);
-        Assert.assertEquals(0, st.count());
+        MyStack<Float> stack;
+        stack = new MyStack<Float>();
+        ArrayList<Float> arr = new ArrayList<Float>();
+
+        for (int i = 0; i < 1000; ++i) {
+            if ((i * 5) % 2 == 0) {
+                float num = (float) Math.random();
+                stack.push(num);
+                arr.add(num);
+            } else {
+                if (arr.size() != 0) {
+                    int size = arr.size() - 1;
+                    float elementArr = arr.remove(size);
+                    float elementStack = stack.pop();
+                    Assert.assertEquals(elementArr, elementStack, 0.0001);
+                }
+            }
+        }
+        Assert.assertEquals(arr.size(), stack.count());
+    }
+
+    //Two tests for strings
+    @Test
+    public void test3() {
+        MyStack<String> stack = new MyStack<String>();
+        stack.push("PushedStringOne");
+        stack.push("PushedStringTwo");
+        stack.pop();
+        stack.pop();
+        try {
+            stack.pop();
+            Assert.fail("Expected NoSuchElementException");
+        } catch (IndexOutOfBoundsException ignored) {
+        }
     }
 
     @Test
-    public void test3() {
+    public void test4() {
+        MyStack<String> stack;
+        stack = new MyStack<String>();
+        ArrayList<String> arr = new ArrayList<String>();
+        for (int i = 1; i < 500; ++i) {
+            if (i % 2 != 0) {
+                stack.push("Stay hungry");
+                arr.add("Stay hungry");
+            } else {
+                stack.push("Stay foolish");
+                arr.add("Stay foolish");
+            }
+        }
+        Assert.assertEquals(arr.size(), stack.count());
+    }
 
-        MyStack st = new MyStack(2);
-        st.push(2);
-        st.push(7);
-        st.push(2);
-        st.push(7);
-        st.push(2);
-        st.push(7);
-        st.push(2);
-        st.push(7);
-        Assert.assertEquals(2, st.count());
+    //Test for my iterator
+    @Test
+    public void test5() {
+        for (int i = 0; i < 300; ++i) {
+            MyStack<Integer> stack = new MyStack<Integer>();
+            ArrayList<Integer> arr = new ArrayList<Integer>();
+            stack.push(i);
+            arr.add(i);
+            Iterator<Integer> MyIter = stack.iterator();
+            Iterator<Integer> ArrIter = arr.iterator();
+            while (MyIter.hasNext()) {
+                Assert.assertEquals(MyIter.next(), ArrIter.next());
+            }
+        }
     }
 }
