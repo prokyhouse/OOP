0dfb590d905d61918b3ff01c49146d0b919bcb97
Кирилл Прокофьев <prokyhouse@mail.ru>
2021-05-13 15:36:59 +0700

    Linter's suggestions code reformat

--- Task_2_1/src/com/proko/Main.java
+++ Task_2_1/src/com/proko/Main.java
@@ -1,77 +1,68 @@
 package com.proko;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import java.util.ArrayList;
-import java.util.Random;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.application.Application;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.animation.KeyFrame;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-
-    GridPane GameGrid = new GridPane();
-    GridPane TextGrid = new GridPane();
-
+    GridPane gameGrid = new GridPane();
+    GridPane textGrid = new GridPane();
     //GAME GRID - 40x40
-    int GridSizeSquared = 40;
-
-    Label GameOver = new Label("");
-    Label Pause = new Label("Нажми любую кнопку, чтобы начать.");
-
-    Label ControlU = new Label("[W/Up]");
-    Label ControlD = new Label("[S/Down]");
-    Label ControlL = new Label("[A/Left] ");
-    Label ControlR = new Label(" [D/Right]");
-
-    Label Close = new Label("Нажми [Escape], чтобы закрыть");
-
-
+    int gridSizeSquared = 40;
+    Label gameOver = new Label("");
+    Label pause = new Label("Нажми любую кнопку, чтобы начать.");
+    Label controlU = new Label("[W/Up]");
+    Label controlD = new Label("[S/Down]");
+    Label controlL = new Label("[A/Left] ");
+    Label controlR = new Label(" [D/Right]");
+    Label close = new Label("Нажми [Escape], чтобы закрыть");
     //ArrayList for the Snake and its body
     //Index 0 of ArrayList is treated as the Head
-    ArrayList<Snake> SnakeP = new ArrayList<>(0);
-    ArrayList<Snake> SnakePC = new ArrayList<>(0);
-
+    ArrayList<Snake> snakeP = new ArrayList<>(0);
+    ArrayList<Snake> snakePC = new ArrayList<>(0);
     //This will be used to keep the game running
-    Timeline Loop;
-
+    Timeline loop;
     //LOOP SPEED
     //1/10 seconds == 10FPS
-    double LoopSpeed = 1 / 10.0;
-
+    double loopSpeed = 1 / 10.0;
     //movementX and movementY, these indicate the direction of the snake head
     //Initial movement is 0
-    int deltaX = 0, deltaY = 0;
-
+    int deltaX = 0;
+    int deltaY = 0;
     //Snakes Head's Initial Position
-    int posX = new Random().nextInt(GridSizeSquared), posY = new Random().nextInt(GridSizeSquared);
-    int pcPosX = new Random().nextInt(GridSizeSquared), pcPosY = new Random().nextInt(GridSizeSquared);
-
+    int posX = new Random().nextInt(gridSizeSquared);
+    int posY = new Random().nextInt(gridSizeSquared);
+    int pcPosX = new Random().nextInt(gridSizeSquared);
+    int pcPosY = new Random().nextInt(gridSizeSquared);
     //Snake Food, same size as snake (Snake.java)
-    Rectangle Food = new Rectangle(12, 12, Color.YELLOW);
-    int FoodPosX = new Random().nextInt(GridSizeSquared);
-    int FoodPosY = new Random().nextInt(GridSizeSquared);
-
+    Rectangle food = new Rectangle(12, 12, Color.YELLOW);
+    int foodPosX = new Random().nextInt(gridSizeSquared);
+    int foodPosY = new Random().nextInt(gridSizeSquared);
     //True = Game is Running
     //False = Game is Paused
     boolean start = false;
     boolean dead = false;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     public void start(Stage PrimaryStage) {
 
@@ -79,47 +70,47 @@ public class Main extends Application {
         FillGrid();
 
         //Constructing Snake's Heads
-        SnakeP.add(new Snake(posX, posY));
-        SnakePC.add(new Snake(pcPosX, pcPosY));
+        snakeP.add(new Snake(posX, posY));
+        snakePC.add(new Snake(pcPosX, pcPosY));
 
-        TextGrid.setVgap(1.5);
-        TextGrid.setHgap(2);
+        textGrid.setVgap(1.5);
+        textGrid.setHgap(2);
 
         //Setting Grid Positions
-        GameGrid.setAlignment(Pos.CENTER);
-        TextGrid.setAlignment(Pos.CENTER);
+        gameGrid.setAlignment(Pos.CENTER);
+        textGrid.setAlignment(Pos.CENTER);
 
         //Initial positions
-        GameGrid.add(Food, FoodPosX, FoodPosY);
-        GameGrid.add(SnakeP.get(0).body, posX, posY);
-        GameGrid.add(SnakePC.get(0).body, pcPosX, pcPosY);
+        gameGrid.add(food, foodPosX, foodPosY);
+        gameGrid.add(snakeP.get(0).body, posX, posY);
+        gameGrid.add(snakePC.get(0).body, pcPosX, pcPosY);
 
         //Adding Text to Grid
-        TextGrid.add(GameOver, 1, 1, 3, 2);
-        TextGrid.add(Pause, 1, 3, 3, 1);
-        TextGrid.add(ControlU, 2, 4, 1, 1);
-        TextGrid.add(ControlL, 1, 5, 1, 1);
-        TextGrid.add(ControlD, 2, 5, 1, 1);
-        TextGrid.add(ControlR, 3, 5, 1, 1);
+        textGrid.add(gameOver, 1, 1, 3, 2);
+        textGrid.add(pause, 1, 3, 3, 1);
+        textGrid.add(controlU, 2, 4, 1, 1);
+        textGrid.add(controlL, 1, 5, 1, 1);
+        textGrid.add(controlD, 2, 5, 1, 1);
+        textGrid.add(controlR, 3, 5, 1, 1);
 
-        TextGrid.add(Close, 1, 6, 3, 1);
+        textGrid.add(close, 1, 6, 3, 1);
 
         //Allows to use both grids in the same screen
-        FlowPane Screen = new FlowPane(Orientation.VERTICAL, GameGrid, TextGrid);
+        FlowPane screen = new FlowPane(Orientation.VERTICAL, gameGrid, textGrid);
 
         //Creating Game Scene with a black background
-        Scene Game = new Scene(Screen);
+        Scene game = new Scene(screen);
 
         //Detects a Key Being Pressed
-        Game.setOnKeyPressed(this::KeyPressedProcess);
+        game.setOnKeyPressed(this::KeyPressedProcess);
 
         //Generates Window
         PrimaryStage.setTitle("ProkoSnake");
-        PrimaryStage.setScene(Game);
+        PrimaryStage.setScene(game);
         PrimaryStage.show();
 
         //Initializing Loop as timeline.
-        Loop = new Timeline(new KeyFrame(Duration.seconds(LoopSpeed),
+        loop = new Timeline(new KeyFrame(Duration.seconds(loopSpeed),
                 event -> {
 
                     //Moves Snake
@@ -127,7 +118,7 @@ public class Main extends Application {
                     MovePCSnake();
                 }));
         // Loop will run endlessly
-        Loop.setCycleCount(Timeline.INDEFINITE);
+        loop.setCycleCount(Timeline.INDEFINITE);
     }
 
     public void MoveUserSnake() {
@@ -138,121 +129,113 @@ public class Main extends Application {
         } else if (deltaY == -1 && (posY == 0)) {
             Die();
             deltaY = 0;
-        } else if (deltaX == 1 && (posX == GridSizeSquared - 1)) {
+        } else if (deltaX == 1 && (posX == gridSizeSquared - 1)) {
             Die();
             deltaX = 0;
-        } else if (deltaY == 1 && (posY == GridSizeSquared - 1)) {
+        } else if (deltaY == 1 && (posY == gridSizeSquared - 1)) {
             Die();
             deltaY = 0;
-        }
-        //No wall crash happens
-        else {
+        } else {
 
             //Updates head position
-            GameGrid.getChildren().remove(SnakeP.get(0).body);
+            gameGrid.getChildren().remove(snakeP.get(0).body);
 
             posX += deltaX;
             posY += deltaY;
-            GameGrid.add(SnakeP.get(0).body, posX, posY);
-            SnakeP.get(0).setPos(posX, posY);
+            gameGrid.add(snakeP.get(0).body, posX, posY);
+            snakeP.get(0).setPos(posX, posY);
 
             //Update for rest of body
-            if (SnakeP.size() > 1) {
-                for (int x = 1; x < SnakeP.size(); x++) {
-                    GameGrid.getChildren().remove(SnakeP.get(x).body);
-                    GameGrid.add(SnakeP.get(x).body, SnakeP.get(x - 1).getOldXpos(), SnakeP.get(x - 1).getOldYpos());
-                    SnakeP.get(x).setPos(SnakeP.get(x - 1).getOldXpos(), SnakeP.get(x - 1).getOldYpos());
+            if (snakeP.size() > 1) {
+                for (int x = 1; x < snakeP.size(); x++) {
+                    gameGrid.getChildren().remove(snakeP.get(x).body);
+                    gameGrid.add(snakeP.get(x).body, snakeP.get(x - 1).getOldXpos(), snakeP.get(x - 1).getOldYpos());
+                    snakeP.get(x).setPos(snakeP.get(x - 1).getOldXpos(), snakeP.get(x - 1).getOldYpos());
                 }
 
             }
 
             //FOOD WAS FOUND
-            if (posX == FoodPosX && posY == FoodPosY) {
+            if (posX == foodPosX && posY == foodPosY) {
                 //Grows Snake duh
-                Grow(SnakeP);
+                Grow(snakeP);
             }
 
             //BODY CRASH
-            for (int x = 1; x < SnakeP.size(); x++) {
-                if (posX == SnakeP.get(x).getXpos() && posY == SnakeP.get(x).getYpos()) {
+            for (int x = 1; x < snakeP.size(); x++) {
+                if (posX == snakeP.get(x).getXpos() && posY == snakeP.get(x).getYpos()) {
                     Die();
                 }
             }
-
-
         }
-
-
     }
 
 
     public void MovePCSnake() {
 
-            //Updates head position
-            GameGrid.getChildren().remove(SnakePC.get(0).body);
+        //Updates head position
+        gameGrid.getChildren().remove(snakePC.get(0).body);
 
-            //Food Distance
-            int xRout = FoodPosX - pcPosX;
-            int yRout = FoodPosY - pcPosY;
+        //Food Distance
+        int xRout = foodPosX - pcPosX;
+        int yRout = foodPosY - pcPosY;
 
-            if (xRout > 0) {
-                pcPosX += 1;
-            } else if (xRout < 0) {
-                pcPosX -= 1;
-            } else if (yRout > 0) {
-                pcPosY += 1;
-            } else if (yRout < 0) {
-                pcPosY -= 1;
+        if (xRout > 0) {
+            pcPosX += 1;
+        } else if (xRout < 0) {
+            pcPosX -= 1;
+        } else if (yRout > 0) {
+            pcPosY += 1;
+        } else if (yRout < 0) {
+            pcPosY -= 1;
+        }
+
+        gameGrid.add(snakePC.get(0).body, pcPosX, pcPosY);
+        snakePC.get(0).setPos(pcPosX, pcPosY);
+
+        //Update for rest of body
+        if (snakePC.size() > 1) {
+            for (int x = 1; x < snakePC.size(); x++) {
+                gameGrid.getChildren().remove(snakePC.get(x).body);
+                gameGrid.add(snakePC.get(x).body, snakePC.get(x - 1).getOldXpos(), snakePC.get(x - 1).getOldYpos());
+                snakePC.get(x).setPos(snakePC.get(x - 1).getOldXpos(), snakePC.get(x - 1).getOldYpos());
             }
 
-            GameGrid.add(SnakePC.get(0).body, pcPosX, pcPosY);
-            SnakePC.get(0).setPos(pcPosX, pcPosY);
+        }
 
-            //Update for rest of body
-            if (SnakePC.size() > 1) {
-                for (int x = 1; x < SnakePC.size(); x++) {
-                    GameGrid.getChildren().remove(SnakePC.get(x).body);
-                    GameGrid.add(SnakePC.get(x).body, SnakePC.get(x - 1).getOldXpos(), SnakePC.get(x - 1).getOldYpos());
-                    SnakePC.get(x).setPos(SnakePC.get(x - 1).getOldXpos(), SnakePC.get(x - 1).getOldYpos());
-                }
+        //FOOD WAS FOUND
+        if (pcPosX == foodPosX && pcPosY == foodPosY) {
+            //Grows Snake duh
+            Grow(snakePC);
+        }
 
+        //BODY CRASH
+        for (int x = 1; x < snakePC.size(); x++) {
+            if (pcPosX == snakePC.get(x).getXpos() && pcPosY == snakePC.get(x).getYpos()) {
+                Die();
             }
-
-            //FOOD WAS FOUND
-            if (pcPosX == FoodPosX && pcPosY == FoodPosY) {
-                //Grows Snake duh
-                Grow(SnakePC);
-            }
-
-            //BODY CRASH
-            for (int x = 1; x < SnakePC.size(); x++) {
-                if (pcPosX == SnakePC.get(x).getXpos() && pcPosY == SnakePC.get(x).getYpos()) {
-                    Die();
-                }
-            }
+        }
     }
 
     //Detects Key Presses
     public void KeyPressedProcess(KeyEvent event) {
         //If you GameOver and Restart
         if (!start && dead && event.getCode() == KeyCode.ENTER) {
-            Pause.setText("Нажмите [Enter], чтобы прервать");
-            GameOver.setText("");
-            Loop.play();
+            pause.setText("Нажмите [Enter], чтобы прервать");
+            gameOver.setText("");
+            loop.play();
             start = true;
             dead = false;
-        }
-        //If Paused and Resumed
-        else if (!start && !dead) {
-            Pause.setText("Нажмите [Enter], чтобы прервать");
-            Loop.play();
+        } else if (!start && !dead) {
+            pause.setText("Нажмите [Enter], чтобы прервать");
+            loop.play();
             start = true;
         }
 
         //If Enter is pressed, game will pause
         if (event.getCode() == KeyCode.ENTER) {
-            Pause.setText("Нажмите любую кнопку, чтобы продолжить");
-            Loop.stop();
+            pause.setText("Нажмите любую кнопку, чтобы продолжить");
+            loop.stop();
             start = false;
         }
 
@@ -284,11 +267,11 @@ public class Main extends Application {
 
     //Fills Grid with rectangles
     public void FillGrid() {
-        for (int x = 0; x < GridSizeSquared; x++) {
-            GameGrid.addColumn(x, new Rectangle(12, 12, Color.GRAY));
+        for (int x = 0; x < gridSizeSquared; x++) {
+            gameGrid.addColumn(x, new Rectangle(12, 12, Color.GRAY));
 
-            for (int y = 1; y < GridSizeSquared; y++)
-                GameGrid.addRow(y, new Rectangle(12, 12, Color.GRAY));
+            for (int y = 1; y < gridSizeSquared; y++)
+                gameGrid.addRow(y, new Rectangle(12, 12, Color.GRAY));
         }
     }
 
@@ -296,14 +279,14 @@ public class Main extends Application {
     public void PlaceFood() {
         Random rPos = new Random();
 
-        int newPosX = rPos.nextInt(GridSizeSquared);
-        int newPosY = rPos.nextInt(GridSizeSquared);
+        int newPosX = rPos.nextInt(gridSizeSquared);
+        int newPosY = rPos.nextInt(gridSizeSquared);
 
-        FoodPosX = newPosX;
-        FoodPosY = newPosY;
+        foodPosX = newPosX;
+        foodPosY = newPosY;
 
-        GameGrid.getChildren().remove(Food);
-        GameGrid.add(Food, newPosX, newPosY);
+        gameGrid.getChildren().remove(food);
+        gameGrid.add(food, newPosX, newPosY);
     }
 
     //Grows Snake's Body
@@ -312,7 +295,7 @@ public class Main extends Application {
         Snake.add(new Snake(Snake.get(Snake.size() - 1).getOldXpos(),
                 Snake.get(Snake.size() - 1).getOldYpos()));
 
-        GameGrid.add(Snake.get(Snake.size() - 1).body,
+        gameGrid.add(Snake.get(Snake.size() - 1).body,
                 Snake.get(Snake.size() - 1).getOldXpos(),
                 Snake.get(Snake.size() - 1).getOldYpos());
 
@@ -323,33 +306,34 @@ public class Main extends Application {
     //Game Over
     public void Die() {
 
-        int size = SnakeP.size();
+        int size = snakeP.size();
 
         //First Removes all but the head from the grid
-        for (int x = size - 1; x > 0; x--)
-            GameGrid.getChildren().remove(SnakeP.get(x).body);
+        for (int x = size - 1; x > 0; x--) {
+            gameGrid.getChildren().remove(snakeP.get(x).body);
+        }
 
         //Removes all but the head from the arrayList
         if (size > 1) {
-            SnakeP.subList(1, size).clear();
+            snakeP.subList(1, size).clear();
         }
 
         //Pauses Game
         start = false;
         dead = true;
-        Loop.stop();
+        loop.stop();
 
-        GameOver.setText("Игра окончена");
-        Pause.setText("Нажмите Enter, чтобы перезапустить игру.");
+        gameOver.setText("Игра окончена");
+        pause.setText("Нажмите Enter, чтобы перезапустить игру.");
 
         //Generates new Position for the snake
-        posX = new Random().nextInt(GridSizeSquared);
-        posY = new Random().nextInt(GridSizeSquared);
+        posX = new Random().nextInt(gridSizeSquared);
+        posY = new Random().nextInt(gridSizeSquared);
 
         //Places Snake in new Position
-        GameGrid.getChildren().remove(SnakeP.get(0).body);
-        GameGrid.add(SnakeP.get(0).body, posX, posY);
-        SnakeP.get(0).setPos(posX, posY);
+        gameGrid.getChildren().remove(snakeP.get(0).body);
+        gameGrid.add(snakeP.get(0).body, posX, posY);
+        snakeP.get(0).setPos(posX, posY);
 
     }
 
